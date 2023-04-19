534e9a727f412ac24c8a2d9fec8359b4e42de90f
=Andrei <balasca.andrei@gmail.com>
2023-04-19 16:55:52 +0300

    finished exercises 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listElement = document.querySelector(`#ingredients`);
+
+const listItem = ingredients.map((ingredient) => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  return item;
+});
+
+listElement.append(...listItem);
