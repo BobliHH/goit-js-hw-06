ced6f098e870d96c27feff60661a8b0aa187da25
=Andrei <balasca.andrei@gmail.com>
2023-04-20 20:47:25 +0300

    finished exercise 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,2 +1,12 @@
 const inputName = document.getElementById("name-input");
 const outputName = document.getElementById("name-output");
+
+const getInputValue = () => {
+  if (inputName.value === "") {
+    outputName.textContent = "Anonymous";
+  } else {
+    outputName.textContent = inputName.value;
+  }
+};
+
+inputName.addEventListener("input", getInputValue);
