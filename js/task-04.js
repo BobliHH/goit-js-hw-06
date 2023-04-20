4c016efafb0f59dfc2f07d4972f6374eb46398b8
=Andrei <balasca.andrei@gmail.com>
2023-04-20 20:04:18 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,5 +1,18 @@
-const counter = 0;
+let counter = 0;
 const value = document.getElementById('value');
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
+
+const onClickDecrementBtn = (subtract) => {
+    counter -= 1;
+    value.textContent = counter;
+}
+
+const onClickIncrementBtn = (add) => {
+  counter += 1;
+  value.textContent = counter;
+};
+
+decrementBtn.addEventListener("click", onClickDecrementBtn);
+incrementBtn.addEventListener("click", onClickIncrementBtn);
 
