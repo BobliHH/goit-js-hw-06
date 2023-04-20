630de134be5b5622f9f3edf1e14aac67cac49fe7
=Andrei <balasca.andrei@gmail.com>
2023-04-20 19:49:42 +0300

    started exercices 4

---
+++ js/task-04.js
@@ -0,0 +1,5 @@
+const counter = 0;
+const value = document.getElementById('value');
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+
