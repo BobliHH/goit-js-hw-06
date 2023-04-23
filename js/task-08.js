db88774fb126dbad4ef3ab12f7308d1528a7df89
=Andrei <balasca.andrei@gmail.com>
2023-04-23 22:43:56 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -3,3 +3,17 @@ const loginForm = document.querySelector("login-form");
 loginForm.addEventListener('submit', submitEvent);
 
 const userProfile = {};
+
+function submitEvent(event){
+
+    const email = event.target.email;
+    const password = event.targe.password;
+
+    if (email.value == '' && password.value == '') {
+        alert('Please fill in all fields')
+    }
+    else {
+        userProfile.email = this.value;
+        userProfile.password = this.value;
+    }
+}
