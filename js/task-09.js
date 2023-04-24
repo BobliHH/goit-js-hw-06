6e14cc34c4a3128c274f8bdb2da1a6e7082aeb4a
=Andrei <balasca.andrei@gmail.com>
2023-04-24 20:45:14 +0300

    started exercise 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+
+const changeColorBtn = document.querySelector('.change-color');
+
+const changeBgColor = () => {
+ body.style.backgroundColor = getRandomHexColor();
+}
+changeColorBtn.addEventListener('click', changeBgColor);
