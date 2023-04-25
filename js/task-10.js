a1dec79e0ba663435504e0d4db5993d8aeea7beb
=Andrei <balasca.andrei@gmail.com>
2023-04-25 13:46:03 +0300

    started exercise 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,9 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.getElementById('controls');
+const inputNumber = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+
